9e7fd58736e5e0c7223a4f1b58e2a3a24a4030f0
milunusu <milanesaruggiero@gmail.com>
2024-04-04 20:29:28 -0400

    ok

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -30,7 +30,7 @@ app.frame('/second', (c) => {
   
   return c.res({
     action: '/first',
-    image: 'https://indigo-advisory-gorilla-502.mypinata.cloud/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/0.jpg',
+    image: 'https://indigo-advisory-gorilla-502.mypinata.cloud/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/1.jpg',
     intents: [
       <Button>alright, rude ass rabbit</Button>,
     ],
@@ -39,3 +39,4 @@ app.frame('/second', (c) => {
 
 export const GET = handle(app)
 export const POST = handle(app)
+ //this better fucking work 
